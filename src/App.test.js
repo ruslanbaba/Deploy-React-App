420e0b003edf9f00a198862e0ed7fe16b31a207e
jasonaustinoshihealth <jasonaustin@oshihealth.com>
2025-06-18 17:12:53 -0400

    added linter

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const x = [1,2,3]
+const y = x.filter(x => x < 2)
+
 test('renders welcome message', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello, DevOps Engineer!/i);
